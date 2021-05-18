a92a0657b9fb85b322b8783846f7919bb5c1e79e
cschaefer26 <c.schaefer.home@gmail.com>
2021-05-18 06:55:57 +0200

    Constant out of hardcoded pad vals.

--- models/tacotron.py
+++ models/tacotron.py
@@ -13,16 +13,16 @@ class Encoder(nn.Module):
     def __init__(self, embed_dims, num_chars, cbhg_channels, K, num_highways, dropout):
         super().__init__()
         self.embedding = nn.Embedding(num_chars, embed_dims)
-        self.pre_net = PreNet(embed_dims)
-        self.cbhg = CBHG(K=K, in_channels=cbhg_channels, channels=cbhg_channels,
-                         proj_channels=[cbhg_channels, cbhg_channels],
-                         num_highways=num_highways)
+        #self.pre_net = PreNet(embed_dims)
+        #self.cbhg = CBHG(K=K, in_channels=cbhg_channels, channels=cbhg_channels,
+        #                 proj_channels=[cbhg_channels, cbhg_channels],
+        #                 num_highways=num_highways)
 
     def forward(self, x):
         x = self.embedding(x)
-        x = self.pre_net(x)
-        x.transpose_(1, 2)
-        x = self.cbhg(x)
+        #x = self.pre_net(x)
+        #x.transpose_(1, 2)
+        #x = self.cbhg(x)
         return x
 
 
